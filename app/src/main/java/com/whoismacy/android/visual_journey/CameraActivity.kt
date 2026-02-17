af899bb3c607cbd57c2258096185ba6092dd3a49
Shem Murimi <murithi.murimi23@students.dkut.ac.ke>
2026-02-17 06:26:24 +0300

    Add Image capture

--- app/src/main/java/com/whoismacy/android/visual_journey/CameraActivity.kt
+++ app/src/main/java/com/whoismacy/android/visual_journey/CameraActivity.kt
@@ -1,29 +1,41 @@
 package com.whoismacy.android.visual_journey
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Camera
 import androidx.compose.material.icons.filled.SwitchCamera
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.whoismacy.android.visual_journey.ui.theme.VisualJourneyTheme
+import com.whoismacy.android.visual_journey.viewmodel.CameraPreviewViewModel
 import com.whoismacy.android.visual_journey.visualjourneyui.CaptureJournalCamera
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.getValue
 
 @AndroidEntryPoint
 class CameraActivity : ComponentActivity() {
@@ -44,7 +56,10 @@ class CameraActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DisplayCamera() {
+    val context = LocalContext.current
+    val viewModel: CameraPreviewViewModel = hiltViewModel()
     val scaffoldState = rememberBottomSheetScaffoldState()
+    val alphaValue = 0.8f
     Box(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -61,18 +76,50 @@ fun DisplayCamera() {
                         .padding(innerPadding),
             ) {
                 CaptureJournalCamera(modifier = Modifier.fillMaxSize())
-                Row(
-                    modifier =
-                        Modifier
-                            .fillMaxWidth()
-                            .padding(top = 32.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
+                Surface(
+                    color = Color.Black.copy(alpha = alphaValue),
                 ) {
-                    IconButton(onClick = {}) {
-                        Icon(
-                            imageVector = Icons.Default.SwitchCamera,
-                            contentDescription = null,
-                        )
+                    Row(
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(top = 32.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                    ) {
+                        IconButton(onClick = {}) {
+                            Icon(
+                                imageVector = Icons.Default.SwitchCamera,
+                                contentDescription = null,
+                                tint = Color.White,
+                            )
+                        }
+                    }
+                }
+                Surface(
+                    color = Color.Black.copy(alpha = alphaValue),
+                    modifier = Modifier.align(Alignment.BottomCenter),
+                ) {
+                    Row(
+                        modifier =
+                            Modifier
+                                .height(120.dp)
+                                .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        IconButton(onClick = {
+                            viewModel.capturePhoto(context)
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.Camera,
+                                contentDescription = null,
+                                tint = Color.White,
+                                modifier =
+                                    Modifier
+                                        .height(48.dp)
+                                        .width(48.dp),
+                            )
+                        }
                     }
                 }
             }
--- app/src/main/java/com/whoismacy/android/visual_journey/viewmodel/CameraPreviewViewModel.kt
+++ app/src/main/java/com/whoismacy/android/visual_journey/viewmodel/CameraPreviewViewModel.kt
@@ -1,44 +1,86 @@
 package com.whoismacy.android.visual_journey.viewmodel
 
 import android.content.Context
+import android.util.Log
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.core.SurfaceRequest
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.lifecycle.awaitInstance
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class CameraPreviewViewModel : ViewModel() {
-    private val _surfaceRequest = MutableStateFlow<SurfaceRequest?>(null)
-    val surfaceRequest: StateFlow<SurfaceRequest?> = _surfaceRequest
+private const val TAG = "CAMERAPREVIEWVIEWMODEL"
 
-    private val cameraPreviewUseCase =
-        Preview.Builder().build().apply {
-            setSurfaceProvider { newSurfaceRequest ->
-                _surfaceRequest.update { newSurfaceRequest }
+@HiltViewModel
+class CameraPreviewViewModel
+    @Inject
+    constructor() : ViewModel() {
+        private val _surfaceRequest = MutableStateFlow<SurfaceRequest?>(null)
+        val surfaceRequest: StateFlow<SurfaceRequest?> = _surfaceRequest
+
+        private val cameraImageCaptureUseCase =
+            ImageCapture
+                .Builder()
+                .apply {
+                    setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                    setFlashMode(ImageCapture.FLASH_MODE_AUTO)
+                }.build()
+
+        private val cameraPreviewUseCase =
+            Preview
+                .Builder()
+                .build()
+                .apply {
+                    setSurfaceProvider { newSurfaceRequest ->
+                        _surfaceRequest.update { newSurfaceRequest }
+                    }
+                }
+
+        suspend fun bindToCamera(
+            appContext: Context,
+            lifeCycleOwner: LifecycleOwner,
+        ) {
+            val processCameraProvider = ProcessCameraProvider.awaitInstance(appContext)
+            processCameraProvider.bindToLifecycle(
+                lifeCycleOwner,
+                CameraSelector.DEFAULT_BACK_CAMERA,
+                cameraPreviewUseCase,
+                cameraImageCaptureUseCase,
+            )
+
+            try {
+                awaitCancellation()
+            } finally {
+                processCameraProvider.unbindAll()
             }
         }
 
-    suspend fun bindToCamera(
-        appContext: Context,
-        lifeCycleOwner: LifecycleOwner,
-    ) {
-        val processCameraProvider = ProcessCameraProvider.awaitInstance(appContext)
-        processCameraProvider.bindToLifecycle(
-            lifeCycleOwner,
-            CameraSelector.DEFAULT_BACK_CAMERA,
-            cameraPreviewUseCase,
-        )
+        fun capturePhoto(context: Context) {
+            val executor = ContextCompat.getMainExecutor(context)
+            cameraImageCaptureUseCase.takePicture(
+                executor,
+                object : ImageCapture.OnImageCapturedCallback() {
+                    override fun onCaptureSuccess(image: ImageProxy) {
+                        super.onCaptureSuccess(image)
+                        image.close()
+                    }
 
-        try {
-            awaitCancellation()
-        } finally {
-            processCameraProvider.unbindAll()
+                    override fun onError(exception: ImageCaptureException) {
+                        super.onError(exception)
+                        exception.message?.let { Log.d(TAG, it) }
+                    }
+                },
+            )
         }
     }
-}
